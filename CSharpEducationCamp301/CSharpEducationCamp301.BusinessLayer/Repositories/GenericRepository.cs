1ad03968ad07e61bb72079726f6ca2d0c515d286
Ayşe Karapinar <164161988+aysekarapinar@users.noreply.github.com>
2025-03-18 02:29:57 +0300

    Add files via upload

---
+++ CSharpEducationCamp301/CSharpEducationCamp301.BusinessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using CSharpEducationCamp301.DataAccessLayer.Abstract;
+using CSharpEducationCamp301.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEducationCamp301.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        CampContext context = new CampContext();
+        private readonly DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _object.Find(id);
+        }
+        public void Insert(T entity)
+        {
+            var addedEntity = context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            var updatedEntity = context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
